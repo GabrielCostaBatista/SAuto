dbc4a64dfd910fa7fab869053b6018ed5e967747
GabrielCostaBatista <gabrielcostabatista@tecnico.ulisboa.pt>
2025-05-12 15:32:23 +0100

    watch running (results we don't know)

--- watch_move/src/watch/scripts/detect_arruco.py
+++ watch_move/src/watch/scripts/detect_arruco.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 
 import rospy
-from fiducial_msgs.msg import FiducialTransformArray
+from aruco_msgs.msg import MarkerArray
+from geometry_msgs.msg import PoseWithCovariance
 
 def marker_callback(msg):
-    for transform in msg.transforms:
-        marker_id = transform.fiducial_id
+    # For marker_publisher, we get a MarkerArray with multiple markers
+    if len(msg.markers) == 0:
+        return
+        
+    for marker in msg.markers:
+        marker_id = marker.id
         # Position of the marker relative to the camera
-        x = transform.transform.translation.x
-        y = transform.transform.translation.y
-        z = transform.transform.translation.z
+        x = marker.pose.pose.position.x
+        y = marker.pose.pose.position.y
+        z = marker.pose.pose.position.z
+        
+        # Print marker information
+        rospy.loginfo(f"Detected marker {marker_id} at position: x={x:.3f}, y={y:.3f}, z={z:.3f}")
         # Use x, y, z for navigation or localization
 
 rospy.init_node('aruco_listener')
-rospy.Subscriber('/fiducial_transforms', FiducialTransformArray, marker_callback)
+rospy.Subscriber('/aruco_marker_publisher/markers', MarkerArray, marker_callback)
 rospy.spin()
